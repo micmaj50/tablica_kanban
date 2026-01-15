edf03c4cfc6a0c46b94ebf7e42364338e7a5455c
Michał Majchrzak <mm57729@zut.edu.pl>
2026-01-15 14:56:59 +0100

    Dodano zależność od eframe i zaimplementowano podstawowy interfejs użytkownika tablicy Kanban.
    - Przebudowano plik main.rs, aby utworzyć proste okno aplikacji.
    - Zaimplementowano podstawowy układ tablicy Kanban z sekcjami „Do zrobienia” i „W trakcie” przy użyciu eframe i egui.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> eframe::Result<()> {
+    eframe::run_native(
+        "Moje Pierwsze Okno", 
+        eframe::NativeOptions::default(), 
+        Box::new(|_cc| Ok(Box::new(MojaAplikacja))) // To jest boilerplate (nieunikniony w Rust)
+    )
+}
+
+struct MojaAplikacja;
+
+impl eframe::App for MojaAplikacja {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Tablica Kanban!");
+
+            ui.horizontal(|ui| {
+                egui::Frame::group(ui.style())
+                .corner_radius(20.0)
+                .show(ui, |ui| {
+                    ui.set_min_size(egui::vec2(200.0, 400.0));
+                    ui.label("ToDo");
+                });
+
+                egui::Frame::group(ui.style())
+                .corner_radius(20.0)
+                .show(ui, |ui| {
+                    ui.set_min_size(egui::vec2(200.0, 400.0));
+                    ui.label("Doing");
+                });
+
+            });
+        });
+    }
 }
